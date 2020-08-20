75b534b641d8998e825bf6deb0e8dfd2f45e250d
fdully <fdully@gmail.com>
2020-08-20 05:35:18 +0300

    adding missing files

---
+++ internal/setup/setup.go
@@ -0,0 +1,66 @@
+package setup
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/fdully/calljournal/internal/logging"
+
+	"github.com/fdully/calljournal/internal/database"
+	"github.com/fdully/calljournal/internal/serverenv"
+	"github.com/fdully/calljournal/internal/storage"
+	"github.com/sethvargo/go-envconfig"
+)
+
+// BlobstoreConfigProvider provides the information about current storage configuration.
+type BlobstoreConfigProvider interface {
+	BlobstoreConfig() *storage.Config
+}
+
+// DatabaseConfigProvider ensures that the environment config can provide a DB config.
+type DatabaseConfigProvider interface {
+	DatabaseConfig() *database.Config
+}
+
+func Setup(ctx context.Context, config interface{}) (*serverenv.ServerEnv, error) {
+	return WithSetup(ctx, config, envconfig.OsLookuper())
+}
+
+func WithSetup(ctx context.Context, config interface{}, l envconfig.Lookuper) (*serverenv.ServerEnv, error) {
+	logger := logging.FromContext(ctx)
+
+	if err := envconfig.ProcessWith(ctx, config, l); err != nil {
+		return nil, fmt.Errorf("failed to load environment variables: %w", err)
+	}
+
+	var serverEnvOpts []serverenv.Option
+
+	if provider, ok := config.(BlobstoreConfigProvider); ok {
+		logger.Info("configuring blobstore")
+
+		storageConfig := provider.BlobstoreConfig()
+
+		blobStore, err := storage.BlobstoreFor(ctx, storageConfig.BlobstoreType)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create blob storage: %w", err)
+		}
+
+		serverEnvOpts = append(serverEnvOpts, serverenv.WithBlobstore(blobStore))
+	}
+
+	if provider, ok := config.(DatabaseConfigProvider); ok {
+		logger.Info("configuring database")
+
+		dbConfig := provider.DatabaseConfig()
+
+		db, err := database.NewDB(ctx, dbConfig)
+		if err != nil {
+			return nil, fmt.Errorf("failed to connect to database: %w", err)
+		}
+
+		// Update serverEnv setup.
+		serverEnvOpts = append(serverEnvOpts, serverenv.WithDatabase(db))
+	}
+
+	return serverenv.NewServerEnv(ctx, serverEnvOpts...), nil
+}
